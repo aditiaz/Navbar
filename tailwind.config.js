5907bc2d90dddb5b76bb8ed3393b8600d3d56655
aditiaz <aditiaan96a@gmail.com>
2022-11-03 20:27:38 +0700

    page 1

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,21 @@ module.exports = {
   // content: ["./public/**/*.{html,js}"],
   // content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "hero-binus": "url('../src/assets/binus.jpeg')",
+      },
+      colors: {
+        orangeB: "#e48118",
+        blueB: "#15B5EA",
+        blueO: "#3E8BCD",
+        footer: "#EBEBEB",
+      },
+      textColor: {
+        orangeT: "#e48118",
+        blueT: "#15B5EA",
+      },
+    },
   },
   plugins: [require("daisyui")],
 };
